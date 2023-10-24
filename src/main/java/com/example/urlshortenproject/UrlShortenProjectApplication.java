cb44af09b586658c6c98e5a02ef73a2b0f3cf708
gimyojun <gywnsdl5623@gmail.com>
2023-10-24 22:56:06 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/urlshortenproject/UrlShortenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlshortenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenProjectApplication.class, args);
+    }
+
+}
